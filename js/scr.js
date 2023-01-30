7bf0a177013a75a1fe06f553bf18a0c33c7abd6b
Самусь Дмитрий <samusdmitrij@gmail.com>
2023-01-30 12:01:10 +0300

    Добавьте файлы проекта.

---
+++ js/scr.js
@@ -0,0 +1,25 @@
+﻿
+setInterval(function () {
+    var now = new Date();
+    var clock = document.getElementById("clock");
+    clock.innerHTML = now.toLocaleTimeString();
+}, 1000)
+
+
+
+
+
+
+document.onreadystatechange = () => {
+    if (document.readyState == "interactive") {
+        var hash = window.location.hash.substr(1);
+        if (hash.length <= 0) {
+            newDock('#Main');
+            //alert(hash + ' '+hash.length + ' 1 ');
+        }
+        else {
+            newDock('#' + hash);
+            //alert(hash + ' ' + hash.length + ' 2 ');
+        }
+    }
+};
